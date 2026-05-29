1db1b38a6c0f8a0b9c6cd786502f941ada05ddf8
Daniel R Medeiros <kabal79@gmail.com>
2026-05-28 22:29:09 -0300

    Pino + http-exception + Helmt +  Active Cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { LoggerModule } from 'nestjs-pino';
+
 import { PrismaModule } from './shared/database/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                target: 'pino-pretty',
+              }
+            : undefined,
+      },
+    }),
+
+    PrismaModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import { Logger } from 'nestjs-pino';
+
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './shared/exceptions/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.enableCors();
+
+  app.useLogger(app.get(Logger));
+
+  app.use(helmet());
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
