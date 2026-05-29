07cdc1afc82fb2c742c4e635f465eedbafa6d071
Daniel R Medeiros <kabal79@gmail.com>
2026-05-28 22:20:53 -0300

    Postgres + Docker + Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './shared/database/prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [PrismaModule],
 })
 export class AppModule {}
